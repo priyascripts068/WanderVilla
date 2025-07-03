d75cca66a34c9ce8d2fd908d5d029cb0d362b6a1
Priya <priya7842612360@gmail.com>
2025-07-04 03:03:20 +0530

    Add root route to redirect to /listings

--- app.js
+++ app.js
@@ -58,9 +58,9 @@ const sessionOptions={
     }
 }
 
-// app.get("/",(req,res)=>{
-//     res.send("Hello world");
-// })
+app.get("/",(req,res)=>{
+    res.redirect("/listings");
+})
 
 app.use(session(sessionOptions));
 app.use(flash());
